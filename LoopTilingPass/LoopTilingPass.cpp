70cdcc7bae34f0b77634376465b7cc0dd85d9a19
Bogdan Stojadinović <bogdanstojadinovic5@gmail.com>
2024-08-14 18:20:03 +0200

    Switched from function to loop pass

--- LoopTilingPass/LoopTilingPass.cpp
+++ LoopTilingPass/LoopTilingPass.cpp
@@ -1,20 +1,35 @@
-#include "llvm/IR/Function.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/Transforms/Utils.h"
+#include "llvm/Transforms/Utils/LoopPeel.h"
+#include "llvm/Transforms/Utils/LoopSimplify.h"
+#include "llvm/Transforms/Utils/LoopUtils.h"
+#include "llvm/Transforms/Utils/SizeOpts.h"
+#include "llvm/Transforms/Utils/UnrollLoop.h"
+#include "llvm/Analysis/LoopAnalysisManager.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/LoopUnrollAnalyzer.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Transforms/Utils/LoopUtils.h"
+#include "llvm/ADT/Statistic.h"
 #include "llvm/Pass.h"
+#include "llvm/Analysis/LoopPass.h"
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/IRBuilder.h"
 
 using namespace llvm;
 
 namespace {
-    struct LoopTilingPass : public FunctionPass {
+    struct LoopTilingPass : public LoopPass {
         static char ID; // Pass identification, replacement for typeid
-        LoopTilingPass() : FunctionPass(ID) {}
+        LoopTilingPass() : LoopPass(ID) {}
 
-        bool runOnFunction(Function &F) override {
-            errs() << "pass test\n";
+        bool runOnLoop(Loop *L, LPPassManager &LPM) override {
+            errs() << "Loop pass test\n";
             return false;
         }
     };
 }
 
 char LoopTilingPass::ID = 0;
-static RegisterPass<LoopTilingPass> X("loop-tiling", "Loop Tiling Pass");
+static RegisterPass<LoopTilingPass> X("loop-tiling", "Loop Tiling Pass", false, false);
